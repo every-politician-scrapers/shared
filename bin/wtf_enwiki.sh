20d73fd685bed8f9099b06a9729afcd2ee08f12e
Tony Bowden <tony@tmtm.com>
2022-01-11 09:28:34 +0200

    Also run wtf-wikipedia against anything in a "wpwatch" file
    As well as extract the infobox etc from pages for people, also fully
    mirror the output from any file listed in any wpwatch file (e.g. page
    for the Cabinet itself, positionholder tables that aren't yet scraped,
    etc)

--- bin/wtf_enwiki.sh
+++ bin/wtf_enwiki.sh
@@ -4,9 +4,21 @@ IFS=$'\n'
 
 mkdir -p enwiki
 
+# Extract core info from people pages (other that QEII
+
 for page in $(fgrep -v Q9682 html/current.csv | qsv select enwiki | qsv search . | qsv dedup | qsv sort | qsv behead); do
   echo $page
   json=$(printf '"%s"' "$page" | xargs wtf_wikipedia)
   pageid=$(printf '%s' "$json" | jq -r .pageID)
   printf '%s' "$json" | jq -r '.sections |= [.[0]] | del(.sections[].references)' > enwiki/$pageid
+done
+
+# Extract all info from pages listed in 'wpwatch' files
+
+for page in $(find . -name 'wpwatch*.txt' -exec egrep -h '^http' {} \+)
+do
+  echo $page
+  json=$(printf '"%s"' "$page" | xargs wtf_wikipedia)
+  pageid=$(printf '%s' "$json" | jq -r .pageID)
+  printf '%s' "$json" | jq -r . > enwiki/$pageid
 done
