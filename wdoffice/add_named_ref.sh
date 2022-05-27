43a420100e52f2c18b4f49ee29eaf495ab885258
Tony Bowden <tony@tmtm.com>
2022-05-27 23:23:48 +0300

    Improve add_named_ref

--- wdoffice/add_named_ref.sh
+++ wdoffice/add_named_ref.sh
@@ -1,29 +1,33 @@
 #!/bin/zsh
 
 SCRAPED_NAME=itemLabel
+
 WIKIDATA_ITEM=item
 WIKIDATA_PSID=psid
 
-wikidata_count=$(qsv search -u -i $1 wikidata.csv | qsv count)
+wcheck=$1
+
+wikidata_count=$(qsv search -u -i $wcheck wikidata.csv | qsv count)
 if [[ $wikidata_count != 1 ]]
 then
-  echo "No unique match to wikidata.csv:"
-  qsv search -u -i $1 wikidata.csv | qsv behead
+  echo "No unique match to wikidata.csv ($wikidata_count):"
+  qsv search -u -i $wcheck wikidata.csv | qsv behead
   return
 fi
-item=$(qsv search -u -i $1 wikidata.csv | qsv select $WIKIDATA_ITEM | qsv behead)
-statementid=$(qsv search -u -i $1 wikidata.csv | qsv select $WIKIDATA_PSID | qsv behead)
+item=$(qsv search -u -i $wcheck wikidata.csv | qsv select $WIKIDATA_ITEM | qsv behead)
+statementid=$(qsv search -u -i $wcheck wikidata.csv | qsv select $WIKIDATA_PSID | qsv behead)
 
-scraped_count=$(qsv search -u -i $1 scraped.csv | qsv count)
+[[ -v 2 ]] && scheck=$2 || scheck=$1
+scraped_count=$(qsv search -u -i $scheck scraped.csv | qsv count)
 if [[ $scraped_count != 1 ]]
 then
   echo "No unique match to scraped.csv:"
-  echo $(qsv search -u -i $1 scraped.csv | qsv behead)
+  qsv search -u -i $scheck scraped.csv | qsv behead
   return
 fi
 
-name=$(qsv search -u -i $1 scraped.csv | qsv select $SCRAPED_NAME | qsv behead)
-claims=$(qsv search -u -i $1 scraped.csv | qsv select $SCRAPED_NAME | qsv behead | qsv fmt --out-delimiter " ")
+name=$(qsv search -u -i $scheck scraped.csv | qsv select $SCRAPED_NAME | qsv behead)
+claims=$(qsv search -u -i $scheck scraped.csv | qsv select $SCRAPED_NAME | qsv behead | qsv fmt --out-delimiter " ")
 
 echo "$statementid $claims"
 echo "$statementid $claims" | xargs wd ar --maxlag 20 add-source-name.js > /dev/null
