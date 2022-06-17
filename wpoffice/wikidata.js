a78625cc3254ab13a21a19e52b34dfe9f4c3de43
Tony Bowden <tony@tmtm.com>
2022-06-17 14:26:05 +0300

    check for alternate_positions

--- wpoffice/wikidata.js
+++ wpoffice/wikidata.js
@@ -6,8 +6,9 @@ module.exports = function () {
   return `SELECT DISTINCT ?item ?itemLabel ?startDate ?endDate ?sourceDate
                (STRAFTER(STR(?held), '/statement/') AS ?psid)
         WHERE {
+          VALUES ?position { wd:${meta.position} ${(meta.alternate_positions || []).map(value => `wd:${value}`).join(' ')} }
           ?item wdt:P31 wd:Q5 ; p:P39 ?held .
-          ?held ps:P39 wd:${meta.position} .
+          ?held ps:P39 ?position .
           FILTER NOT EXISTS { ?held wikibase:rank wikibase:DeprecatedRank }
 
           {
