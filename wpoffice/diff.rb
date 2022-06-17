9335251871728a78414136a503bba45be8f1e39c
Tony Bowden <tony@tmtm.com>
2022-06-17 12:14:39 +0300

    wpoffice: ignore diffs where WP has better date precision

--- wpoffice/diff.rb
+++ wpoffice/diff.rb
@@ -3,5 +3,9 @@
 
 require 'every_politician_scraper/comparison'
 
-diff = EveryPoliticianScraper::NulllessComparison.new('wikidata.csv', 'scraped.csv').diff
+# TODO: pass the list of decorators as an argument
+diff = EveryPoliticianScraper::Comparison.new('wikidata.csv', 'scraped.csv').diff
+diff = DaffDiff::Decorator.new(data: diff, cell_class: DaffDiff::Decorator::DatePrecision).decorated
+diff = DaffDiff::Decorator.new(data: diff, cell_class: DaffDiff::Decorator::Nullless).decorated
+
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
